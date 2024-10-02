f57b49ea4ea125b9aaa396b0b498fcf3bcb6044c
angel <angel@DESKTOP-BFVQGAU.dhcp.wustl.edu>
2024-10-02 14:16:46 -0500

    finished code

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,19 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+StdDraw.setCanvasSize(600, 400);
+        
+        
+        StdDraw.setPenColor(Color.BLUE);
+        StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.5); 
+
+        StdDraw.setPenColor(Color.WHITE);
+        StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.15);  
+        
+        StdDraw.setPenColor(Color.RED);
+        StdDraw.filledCircle(0.1, 0.8, 0.1);
+    
 	}
 }
+
+
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -8,7 +8,7 @@ import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
-
+import java.awt.Color;
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
@@ -19,6 +19,30 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
 		
+		Color color = new Color(redComponent, greenComponent, blueComponent);
+        StdDraw.setPenColor(color);
+		
+		boolean isFilled = in.nextBoolean();
+		
+		if (shapeType.equals("rectangle")) {
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			double x = parameterOne;
+			double y = parameterTwo;
+			double halfWidth = parameterThree;
+			double halfHeight = parameterFour;
+			if (isFilled) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			} else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
 	}
 }
